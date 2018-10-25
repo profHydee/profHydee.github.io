835b1da6abfcd00c6c114fe2f0870a3b8a234dad
profHydee <idrisadegbite7@gmail.com>
2018-10-25 10:42:36 -0400

    javascript

---
+++ js/test.js
@@ -0,0 +1,17 @@
+var a = 8;
+var b = 10;
+var c = 12;
+var d = 13;
+var e = 80;
+
+console.log(a + b);
+alert(a + b * c);
+document.write(d * b / c + "<br/>");
+document.write("I have " + c + " girlfriends and they all love me");
+document.write("<br/>" + a % e);
+document.write("<br/>Buhari is " + e + " years old<br/>")
+alert(a + b + c / e - d)
+document.getElementById("testing").innerHTML = "I am testing my JavaScript as a beginner";
+document.getElementById("testing").innerHTML = "I think i am getting it";
+document.getElementById("testing").innerHTML = "Its a gradual process, i know";
+document.getElementById("testing").innerHTML = "I will murder JS when i become expert";
